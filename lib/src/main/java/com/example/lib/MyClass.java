7cd66e55ba6ed029849f7a6456566475f48328c5
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2020-01-03 15:01:48 +0800

    Update MyClass.java

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -2,34 +2,20 @@ package com.example.lib;
 import java.util.Scanner;
 public class MyClass {
 
-    public static void main(String[] args) {
-        int Year=0,Month=0,Day=0,TotalDay=0;
-        int MonthDay[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        boolean LeapYear=false;
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("請輸入西元年 月 日");
-        Year = scanner.nextInt();
-        Month = scanner.nextInt();
-        Day = scanner.nextInt();
-        if(Year%4!=0)
-            LeapYear=false;
-        else if(Year%100!=0)
-            LeapYear=true;
-        else if(Year%400!=0)
-            LeapYear=false;
-        else
-            LeapYear=true;
-        if(LeapYear==true)
-            MonthDay[1]=29;
-        if(Month>12)
-            System.out.println("請輸入正確月份!");
-        else if(MonthDay[Month-1]<Day)
-            System.out.printf("輸入錯誤!!%d年的%d月只有%d天\n",Year,Month,MonthDay[Month-1]);
-        else {
-            for(int i=Month-2;i>=0;--i){
-                TotalDay+=MonthDay[i];
-            }
-            TotalDay+=Day;
-            System.out.printf("這天是今年第%d天\n",TotalDay);
+        String LuckyString[] = {"普通","吉","大吉"};
+        System.out.println("請輸入生日 :月 日");
+        int MonthNumber = Integer.parseInt(scanner.next());
+        while (MonthNumber<1||MonthNumber>12){
+            System.out.println("輸入錯誤請再輸入月份:");
+            MonthNumber = Integer.parseInt(scanner.next());
         }
+        int DateNumber = Integer.parseInt(scanner.next());
+        while (DateNumber<1||DateNumber>31){
+            System.out.println("輸入錯誤請再輸入日期:");
+            DateNumber = Integer.parseInt(scanner.next());
+        }
+        int LuckyNumber = (MonthNumber*2+DateNumber)%3;
+        System.out.println("你的運勢:"+LuckyString[LuckyNumber]);
     }}
